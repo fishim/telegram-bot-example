988c73b632b0ff7ca2c5b336964e071950104e0d
Антон <antonolyshevets@gmail.com>
2023-04-14 14:04:25 +0300

    добавив коментарі

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 import telebot
 
-bot = telebot.TeleBot('5831486734:AAGLPj77zzky_CZO7cvygiFnnT7GwmJyUaY')
+bot = telebot.TeleBot('5831486734:AAGLPj77zzky_CZO7cvygiFnnT7GwmJyUaY')#вказуємо токен бота 
 
 @bot.message_handler(commands=['start'])
-def main(message):
+def main(message):                                         #функція для повернення команди 
     bot.send_message(message.chat.id, message.text)
 
 @bot.message_handler(func=lambda message: True)
-def echo_all(message):
+def echo_all(message):                                     #функція для повернення повідомлення
 	bot.send_message(message.chat.id, message.text)
 
 bot.infinity_polling()
